e5c3cd5301f3ace1a24971b9d919d5516a44b145
Micah Gideon Modell <micah.modell@gmail.com>
2017-06-22 17:27:56 +0900

    Working on making Devise get me OAuth privs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+  
+  end
 
-  def load; end
+  def load
+    credentials = request.env[ 'omniauth.auth' ][ 'credentials' ]
+    session = GoogleDrive::Session.from_access_token( credentials[ 'token ' ] )
+    myspreadsheets = session.spreadsheets
+  
+  end
 end
